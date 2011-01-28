5828906434dbaf816372154414b2e2145ebc54ac
frost-nzcr4 <frost-nzcr4@gmail>
2011-01-29 00:02:34 +0300

    Fullscreen plugin
    - Borders restored in Chrome
    - View HTML source now works
    - iframe stretched to fullscreen

--- plugins/wysiwyg.fullscreen.js
+++ plugins/wysiwyg.fullscreen.js
@@ -16,27 +16,71 @@
 		version: "",
 		defaults: {
 			css: {
-				width:	"100%",
-				height:	"100%",
-				position: "absolute",
-				left:	"0px",
-				top:	"0px",
-				background: "rgb(255, 255, 255)",
-				padding: "0px",
-				border: "0px",
-				"z-index": 1000
+				editor: {
+					width:	"100%",
+					height:	"100%"
+				},
+				element: {
+					width:	"100%",
+					height:	"100%",
+					position: "absolute",
+					left:	"0px",
+					top:	"0px",
+					background: "rgb(255, 255, 255)",
+					padding: "0px",
+					"border-bottom-color": "",
+					"border-bottom-style": "",
+					"border-bottom-width": "0px",
+					"border-left-color": "",
+					"border-left-style": "",
+					"border-left-width": "0px",
+					"border-right-color": "",
+					"border-right-style": "",
+					"border-right-width": "0px",
+					"border-top-color": "",
+					"border-top-style": "",
+					"border-top-width": "0px",
+					"z-index": 1000
+				},
+				original: {
+					width:	"100%",
+					height:	"100%",
+					position: "absolute",
+					left:	"0px",
+					top:	"0px",
+					background: "rgb(255, 255, 255)",
+					padding: "0px",
+					"border-bottom-color": "",
+					"border-bottom-style": "",
+					"border-bottom-width": "0px",
+					"border-left-color": "",
+					"border-left-style": "",
+					"border-left-width": "0px",
+					"border-right-color": "",
+					"border-right-style": "",
+					"border-right-width": "0px",
+					"border-top-color": "",
+					"border-top-style": "",
+					"border-top-width": "0px",
+					"z-index": 1000
+				}
 			}
 		},
 		originalBoundary: {
-			width: null,
-			height: null
+			editor: {
+				width: null	// check state by width?
+			},
+			element: {
+			},
+			original: {
+			}
 		},
 
 		init: function (Wysiwyg, options) {
 			options = options || {};
 			options = $.extend(true, this.defaults, options);
 
-			if (this.originalBoundary.width) {
+			if (this.originalBoundary.element.width) {
 				this.restore(Wysiwyg);
 			} else {
 				this.stretch(Wysiwyg, options);
@@ -46,22 +90,50 @@
 		restore: function (Wysiwyg) {
 			var propertyName;
 
-			for (propertyName in this.defaults.css) {
-				Wysiwyg.element.css(propertyName, this.originalBoundary[propertyName]);
+			for (propertyName in this.defaults.css.editor) {
+				Wysiwyg.editor.css(propertyName, this.originalBoundary.editor[propertyName]);
+				this.originalBoundary.editor[propertyName] = null;
+			}
 
-				this.originalBoundary[propertyName] = null;
+			for (propertyName in this.defaults.css.element) {
+				Wysiwyg.element.css(propertyName, this.originalBoundary.element[propertyName]);
+				this.originalBoundary.element[propertyName] = null;
+			}
+
+			for (propertyName in this.defaults.css.original) {
+				$(Wysiwyg.original).css(propertyName, this.originalBoundary.original[propertyName]);
+				this.originalBoundary.original[propertyName] = null;
 			}
 		},
 
 		stretch: function (Wysiwyg, options) {
 			var propertyName;
 
-			for (propertyName in this.defaults.css) {
-				this.originalBoundary[propertyName] = Wysiwyg.element.css(propertyName);
+			// save previous values
+			for (propertyName in this.defaults.css.editor) {
+				this.originalBoundary.editor[propertyName] = Wysiwyg.editor.css(propertyName);
 			}
 
-			for (propertyName in this.defaults.css) {
-				Wysiwyg.element.css(propertyName, options.css[propertyName]);
+			for (propertyName in this.defaults.css.element) {
+				this.originalBoundary.element[propertyName] = Wysiwyg.element.css(propertyName);
+			}
+
+			for (propertyName in this.defaults.css.original) {
+				this.originalBoundary.original[propertyName] = $(Wysiwyg.original).css(propertyName);
+			}
+
+			// set new values
+			for (propertyName in this.defaults.css.editor) {
+				Wysiwyg.editor.css(propertyName, options.css.editor[propertyName]);
+			}
+
+			for (propertyName in this.defaults.css.element) {
+				Wysiwyg.element.css(propertyName, options.css.element[propertyName]);
+			}
+
+			options.css.original.top = Wysiwyg.ui.panel.css("height");
+			for (propertyName in this.defaults.css.original) {
+				$(Wysiwyg.original).css(propertyName, options.css.original[propertyName]);
 			}
 		}
 	};
