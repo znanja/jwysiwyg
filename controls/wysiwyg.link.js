beb571cc8033f03ddd5c50df79c9af2b8f998d90
frost-nzcr4 <frost-nzcr4@gmail>
2011-02-04 15:44:11 +0300

    Replace \s(?<!\n)\n to \n

--- controls/wysiwyg.link.js
+++ controls/wysiwyg.link.js
@@ -22,14 +22,14 @@
 			var self = Wysiwyg, elements, dialog, szURL, a, selection,
 				formLinkHtml, formTextLegend, formTextUrl, formTextTitle, formTextTarget,
 				formTextSubmit, formTextReset;
-	
+
 			formTextLegend  = "Insert Link";
 			formTextUrl     = "Link URL";
 			formTextTitle   = "Link Title";
 			formTextTarget  = "Link Target";
 			formTextSubmit  = "Insert Link";
 			formTextReset   = "Cancel";
-	
+
 			if ($.wysiwyg.i18n) {
 				formTextLegend = $.wysiwyg.i18n.t(formTextLegend, "dialogs");
 				formTextUrl    = $.wysiwyg.i18n.t(formTextUrl, "dialogs");
@@ -38,39 +38,39 @@
 				formTextSubmit = $.wysiwyg.i18n.t(formTextSubmit, "dialogs");
 				formTextReset  = $.wysiwyg.i18n.t(formTextReset, "dialogs");
 			}
-	
-			formLinkHtml = '<form class="wysiwyg"><fieldset><legend>' + formTextLegend + '</legend>' + 
+
+			formLinkHtml = '<form class="wysiwyg"><fieldset><legend>' + formTextLegend + '</legend>' +
 				'<label>' + formTextUrl + ': <input type="text" name="linkhref" value=""/></label>' +
 				'<label>' + formTextTitle + ': <input type="text" name="linktitle" value=""/></label>' +
-				'<label>' + formTextTarget + ': <input type="text" name="linktarget" value=""/></label>' + 
+				'<label>' + formTextTarget + ': <input type="text" name="linktarget" value=""/></label>' +
 				'<input type="submit" class="button" value="' + formTextSubmit + '"/> ' +
 				'<input type="reset" value="' + formTextReset + '"/></fieldset></form>';
-	
+
 			a = {
 				self: self.dom.getElement("a"), // link to element node
 				href: "http://",
 				title: "",
 				target: ""
 			};
-	
+
 			if (a.self) {
 				a.href = a.self.href ? a.self.href : a.href;
 				a.title = a.self.title ? a.self.title : "";
 				a.target = a.self.target ? a.self.target : "";
 			}
-	
+
 			if ($.fn.dialog) {
 				elements = $(formLinkHtml);
 				elements.find("input[name=linkhref]").val(a.href);
 				elements.find("input[name=linktitle]").val(a.title);
 				elements.find("input[name=linktarget]").val(a.target);
-	
+
 				if ($.browser.msie) {
 					dialog = elements.appendTo(Wysiwyg.editorDoc.body);
 				} else {
 					dialog = elements.appendTo("body");
 				}
-	
+
 				dialog.dialog({
 					modal: true,
 					width: self.defaults.formWidth,
@@ -82,7 +82,7 @@
 								szURL = $('input[name="linkhref"]', dialog).val(),
 								title = $('input[name="linktitle"]', dialog).val(),
 								target = $('input[name="linktarget"]', dialog).val();
-	
+
 							if (a.self) {
 								if ("string" === typeof (szURL)) {
 									if (szURL.length > 0) {
@@ -93,26 +93,26 @@
 									}
 								}
 							} else {
-	
+
 								if ($.browser.msie) {
 									self.ui.returnRange();
 								}
-	
+
 								//Do new link element
 								selection = self.getInternalRange();
-	
-	
+
+
 								if (selection.toString) {
 									selection = selection.toString();
 								} else if (selection.text) {	// IE
 									selection = selection.text;
 								}
-	
+
 								if (selection && selection.length > 0) {
 									if ($.browser.msie) {
 										self.ui.focus();
 									}
-	
+
 									if ("string" === typeof (szURL)) {
 										if (szURL.length > 0) {
 											self.editorDoc.execCommand("createLink", false, szURL);
@@ -152,20 +152,20 @@
 				} else {
 					//Do new link element
 					selection = self.getInternalRange();
-	
+
 					if (selection.toString) {
 						selection = selection.toString();
 					} else if (selection.text) {	// IE
 						selection = selection.text;
 					}
-	
+
 					if (selection && selection.length > 0) {
 						if ($.browser.msie) {
 							self.ui.focus();
 							self.editorDoc.execCommand("createLink", true, null);
 						} else {
 							szURL = window.prompt(formTextUrl, a.href);
-	
+
 							if ("string" === typeof (szURL)) {
 								if (szURL.length > 0) {
 									self.editorDoc.execCommand("createLink", false, szURL);
