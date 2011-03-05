27455b0986c91521d23bc4b597501708a2c1be8c
frost-nzcr4 <frost-nzcr4@gmail>
2011-03-06 00:05:54 +0300

    more informative

--- help/bin/compile.bash
+++ help/bin/compile.bash
@@ -15,6 +15,19 @@ jwysiwyg_help() {
 	echo
 	echo "Usage: $NAME full [outfile] </path/to/compile.conf"
 	echo
+	echo "Compile all js files into new one"
+	echo "    (default: jquery.wysiwyg.full.js)"
+	echo "and try to compress it with yuicompressor"
+	echo "    (default: jquery.wysiwyg.full.min.js)"
+	echo
+}
+
+jwysiwyg_help_compressor() {
+	echo
+	echo "To minimize file:"
+	echo "1. Download YUI Compressor [http://yuilibrary.com/downloads/#yuicompressor]"
+	echo "2. Extract and copy file from ./build/yuicompressor-2.4.2.jar to jwysiwyg/help/bin/yuicompressor-2.4.2.jar"
+	echo
 }
 
 jwysiwyg_title() {
@@ -46,20 +59,32 @@ case $1 in
 			echo > $outfile
 		fi
 
+		echo "Read config..."
 		while read path; do
+			echo "+ Read $path..."
 			for filename in $(find $JWYSIWYG_ROOT_DIR/$path); do
 				jwysiwyg_title $filename $outfile
 			done
 		done
 
 		if [ ! -e "$JWYSIWYG_BIN_DIR/yuicompressor-2.4.2.jar" ]; then
-			echo -e "\nyuicompressor-2.4.2.jar not found\n"
-			echo "To minimize file:"
-			echo "1. Download YUI Compressor [http://yuilibrary.com/downloads/#yuicompressor]"
-			echo "2. Extract and copy file from ./build/yuicompressor-2.4.2.jar to jwysiwyg/help/bin/yuicompressor-2.4.2.jar"
+			echo -e "\nyuicompressor-2.4.2.jar not found"
+			jwysiwyg_help_compressor
 		else
-			java -jar $JWYSIWYG_BIN_DIR/yuicompressor-2.4.2.jar $outfile -o "${outfile%"js"}min.js" --charset utf-8 --line-break 500 --type js
+			minified=${outfile%"js"}min.js
+
+			if [ ! -e "$minified" ]; then
+				echo "File $minified not exists. Create..."
+				touch $minified
+			else
+				echo "File $minified exists. Clear..."
+				echo > $minified
+			fi
+
+			java -jar $JWYSIWYG_BIN_DIR/yuicompressor-2.4.2.jar $outfile -o $minified --charset utf-8 --line-break 500 --type js
 		fi
+
+		echo "Done"
 	;;
 	help)
 		jwysiwyg_help
