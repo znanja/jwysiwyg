a03943b24626b4a64403b1a9c498efdeaa26a67f
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2011-05-04 15:46:34 +0400

    Version bump to 0.97

--- jquery.wysiwyg.js
+++ jquery.wysiwyg.js
@@ -1,6 +1,6 @@
 /**
- * WYSIWYG - jQuery plugin 0.96
- * (Pretty girl)
+ * WYSIWYG - jQuery plugin 0.97
+ * (Next step)
  *
  * Copyright (c) 2008-2009 Juan M Martinez, 2010-2011 Akzhan Abdulin and all contributors
  * https://github.com/akzhan/jwysiwyg
