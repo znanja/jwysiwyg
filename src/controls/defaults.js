563e1e37ffd524937b497c0305f93f18caecaf5a
Brent Kirby <brent@kurbmedia.com>
2011-05-12 02:09:11 -0400

    added jasmine for spec tests

---
+++ src/controls/defaults.js
@@ -0,0 +1,30 @@
+// Default wysiwyg controls list
+
+Wysiwyg.controls.bold 	   = {};
+Wysiwyg.controls.highlight = {};
+Wysiwyg.controls.italic    = {};
+Wysiwyg.controls.link  	   = {};
+
+Wysiwyg.controls.h1 	   = {};
+Wysiwyg.controls.h2 	   = {};
+Wysiwyg.controls.h3 	   = {};
+Wysiwyg.controls.h4 	   = {};
+Wysiwyg.controls.indent    = {};
+Wysiwyg.controls.outdent   = {};
+
+Wysiwyg.controls.paste	= {};
+Wysiwyg.controls.undo	= {};
+Wysiwyg.controls.redo	= {};
+
+Wysiwyg.controls.insertHorizontalRule = {};
+Wysiwyg.controls.insertImage 		  = {};
+Wysiwyg.controls.insertOrderedList 	  = {};
+Wysiwyg.controls.insertUnorderedList  = {};
+Wysiwyg.controls.justifyRight 		  = {};
+Wysiwyg.controls.justifyFull  		  = {};
+Wysiwyg.controls.justifyRight  		  = {};
+Wysiwyg.controls.strikeThrough  	  = {};
+
+Wysiwyg.controls.subscript		= {};
+Wysiwyg.controls.supercscript	= {};
+Wysiwyg.controls.underline		= {};
--- src/core.js
+++ src/core.js
@@ -1,24 +1,30 @@
 // Global wysiwyg object.
 var Wysiwyg = function(){
 	
-	this.version =  "@VERSION";		// Overwritten at compile time
-	this.fn      = {}; 				// Plugins are accessed via fn caller, this is compatabile with jQuery's format and is familiar.
-	this.dialog  = {};
-	this.utils   = {};
-	this.ui      = {};
+	this.version  =  "@VERSION";		// Overwritten at compile time
+	this.fn       = {}; 				// Plugins are accessed via fn caller, this is compatabile with jQuery's format and is familiar.
+	this.dialog   = {};
+	this.utils    = {};
+	this.ui       = {};
+	this.controls = {};
 	
 	this.activeEditor = null;
 	this.init		  = function (object, options) {}; // call instance
 	this.instance     = function (options) {}; // create new object
 
-	this.activeEditor: null;        // References the active editor instance, useful for having a global toolbar.
-	this.instances: [];             // Collection
+	this.activeEditor = null;        // References the active editor instance, useful for having a global toolbar.
+	this.instances    = [];             // Collection
 	
 	//
 	// Use extend to add plugins or functionality that isn't part of "core". 
 	// Extending sets up a proper fn namespace, and allows plugins to initialize, 
 	// gives them access to core libs, a 'activeEditor' instance, and hooks.
-	// 
+	//  
+	// Example: 
+	// 	var myPlugin = {};
+	//	$.wysiwyg.extend('myPlugin', myPlugin);
+	//
+	//
 	this.extend  = function( namespace, plugin ){
 		
 		$.extend( plugin, pluginApi );
@@ -29,8 +35,13 @@ var Wysiwyg = function(){
 		// Fire the plugin "init" method if it exists, we pass the core Wysiwyg object
 		// in the event it needs access to various functionality.
 		
-		if ( jQuery.isFunction( plugin.init ) ) {
-			plugin.init.call(this);
+		if ( jQuery.isFunction( this.fn[namespace] ) ) {
+			this.fn[namespace].init.call(this);
+		}
+		
+		// If the plugin defines a control internally, add it to the available controls.
+		if ( this.fn[namespace].control ) {
+			$.extend(this.controls, this.fn[namespace].control);
 		}
 	};
 	
@@ -39,8 +50,8 @@ var Wysiwyg = function(){
 	// This also provides a standardized interface for plugins.
 	
 	var pluginApi = {
-		activeEditor: function(w){ w.activeEditor}(this), // Make sure this is up to date on extend?
-		init: 		  function(){}		
+		activeEditor: function(w){ w.activeEditor; }(this), // Make sure this is up to date on extend?
+		init: 		  function(w){}
 	};
 	
 	
