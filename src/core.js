44b7619be2e1bbe164ddbcd979bbdc8ac7d060a9
Akzhan Abdulin <akzhan.abdulin@gmail.com>
2011-05-13 18:14:22 +0400

    Ideas to realize $.fn and plugins.

--- src/core.js
+++ src/core.js
@@ -1,58 +1,77 @@
 // Global wysiwyg object.
-var Wysiwyg = function(){
-	
-	this.version  =  "@VERSION";		// Overwritten at compile time
-	this.fn       = {}; 				// Plugins are accessed via fn caller, this is compatible with jQuery's format and is familiar.
+var Wysiwyg, EditorInstance; // forward declaration
+
+Wysiwyg = function(){
+
+	// Class methods are accessed directly as members of Wysiwyg
+	// Plugins are accessed directly as members of Wysiwyg
+	this.version  =  "@VERSION"; // Overwritten at compile time
+	this.fn       =  EditorInstance.prototype; // fn are instance methods.
 	this.dialog   = {};
 	this.utils    = {};
 	this.ui       = {};
 	this.controls = {};
 	
 	this.activeEditor = null;
-	this.init		  = function (object, options) {}; // call instance
+	this.init         = function (obj, options) {}; // call instance
 	this.instance     = function (options) {}; // create new object
 
 	this.activeEditor = null;        // References the active editor instance, useful for having a global toolbar.
 	this.instances    = [];             // Collection
 	
-	//
-	// Use extend to add plugins or functionality that isn't part of "core". 
-	// Extending sets up a proper fn namespace, and allows plugins to initialize, 
-	// gives them access to core libs, a 'activeEditor' instance, and hooks.
-	//  
-	// Example: 
-	// 	var myPlugin = {};
-	//	$.wysiwyg.extend('myPlugin', myPlugin);
-	//
-	//
-	this.extend  = function( namespace, plugin ){
-		
-		$.extend( plugin, pluginApi );
-		
-		// Place the new plugin in the fn namespace
-		this.fn[namespace] = plugin;
-
-		// Fire the plugin "init" method if it exists, we pass the core Wysiwyg object
-		// in the event it needs access to various functionality.
-		
-		if ( jQuery.isFunction( this.fn[namespace] ) ) {
-			this.fn[namespace].init.call(this);
-		}
-		
-		// If the plugin defines a control internally, add it to the available controls.
-		if ( this.fn[namespace].control ) {
-			$.extend(this.controls, this.fn[namespace].control);
-		}
-	};
 	
 	// The plugin API provides defaults for plugin functionality. When creating plugins
 	// they simply override these methods. Any methods not overridden keep these defaults. 
 	// This also provides a standardized interface for plugins.
 	
 	var pluginApi = {
-		activeEditor: function(w){ w.activeEditor; }(this), // Make sure this is up to date on extend?
-		init: 		  function(w){}
+		getActiveEditor: function(w) { Wysiwyg.activeEditor; },
+		init:            function(w) {},
+		isPlugin:        true
 	};
-	
-	
+
+	//
+	// Use registerPlugin to add plugins or functionality that isn't part of "core". 
+	// Extending sets up a proper name, and allows plugins to initialize, 
+	// gives them access to core libs, and hooks.
+  // Active editor should be accessed through $.wysiwyg.activeEditor.
+
+	// Example:
+	// 	var myPlugin = {};
+	//	$.wysiwyg.registerPlugin('myPlugin', myPlugin);
+	//
+	this.registerPlugin = function( name, plugin ) {
+		
+		$.extend( plugin, pluginApi );
+		
+		// Place the new plugin in the core namespace
+		this[name] = $.extend({}, pluginApi, plugin); //  plugin can override pluginApi defaults.
+
+		// Fire the plugin "init" method (it is always exists due to pluginApi declaration, we pass the core Wysiwyg object
+		// in the event it needs access to various functionality.
+		
+		this.fn[name].init.call(this);
+		
+		// If the plugin defines a control internally, add it to the available controls.
+		if ( 'control' in this.fn[name] ) {
+			$.extend(this.controls, this.fn[name].control);
+		}
+
+	};
+
+	this.plugins = function() {
+		var r = {};
+		$.each($.fn, function(item, key) {
+			if ('isPlugin' in this)
+			{
+				r[key] = this;
+			}
+		});
+		return r;
+	};
+
+	// active editor instance extend itself using Wysiwyg.fn (prototyping)
+  EditorInstance = function()
+  {
+  };
 };
