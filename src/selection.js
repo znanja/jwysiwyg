1c117d4b7ee9cdb5521371ea97e4abd27a65d939
Brent Kirby <brent@kurbmedia.com>
2011-05-14 03:55:43 -0400

    stub editor commands

---
+++ src/selection.js
@@ -0,0 +1,14 @@
+//
+// Functions for handling user selection data
+//
+// Gets the current selection range
+//
+Wysiwyg.fn.getRange = function(){
+	
+};
+
+// Gets the current user selection
+//
+Wysiwyg.fn.getSelection = function(){
+	
+};
