786c2bdb8fc1d18a3c20b86ac83d317932b565fd
Brent Kirby <brent@kurbmedia.com>
2011-05-15 02:24:40 -0400

    create global debug file, merge in console

--- src/debug.js
+++ src/debug.js
@@ -1,10 +1,14 @@
-var console = ('console' in window) ? window.console : {
-	log: $.noop,
-	error: $.noop,
-	warn: $.noop,
-	info: $.noop,
-	time: $.noop,
-	timeEnd: $.noop,
-	assert: $.noop
-};
-Wysiwyg.console = console;
+if ( @DEBUG ){
+	
+	var console = ('console' in window) ? window.console : {
+		log: $.noop,
+		error: $.noop,
+		warn: $.noop,
+		info: $.noop,
+		time: $.noop,
+		timeEnd: $.noop,
+		assert: $.noop
+	};
+	
+	Wysiwyg.console = console;
+}
