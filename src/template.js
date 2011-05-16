b64ead1740dd07548106f4f9aa058ff3645095a4
Brent Kirby <brent@kurbmedia.com>
2011-05-16 01:00:03 -0400

    update template to use .extend. add raw caching

--- src/template.js
+++ src/template.js
@@ -1,84 +1,82 @@
 // Basic templating functionality.
 
-Wysiwyg.template     = {};
-Wysiwyg.templateData = {};
+Wysiwyg.extend('template', {
+	// Stores a cache of all raw loaded template data (vs store which is specific templates)
+	cache: {},
+	// Parse a html template using object style syntax.
+	//	
+	parse: function( tplString, data ){
+		var tmpl, func,
+			settings = {
+				evaluate    : /\{([\s\S]+?)\}/g,
+				interpolate : /\{=([\s\S]+?)\}>/g
+			};
 
-// Parse a html template using object style syntax.
-//
-Wysiwyg.template.parse = function(tplString, data){
-	var tmpl, func,
-		settings = {
-			evaluate    : /\{([\s\S]+?)\}/g,
-			interpolate : /\{=([\s\S]+?)\}>/g
-		};
-	
-	//
-	// JavaScript micro-templating, similar to John Resig's implementation.
-	// Default implementation by underscore.js.
-	// https://github.com/documentcloud/underscore
-	//
-	tmpl = 'var __p=[],print=function(){__p.push.apply(__p,arguments);};' +
-		'with(obj||{}){__p.push(\'' +
-		str.replace(/\\/g, '\\\\')
-	        .replace(/'/g, "\\'")
-	        .replace( settings.interpolate, function( match, code ) {
-	          return "'," + code.replace(/\\'/g, "'") + ",'";
-	        })
-	        .replace( settings.evaluate || null, function( match, code ) {
-	          return "');" + code.replace(/\\'/g, "'")
-	                             .replace(/[\r\n\t]/g, ' ') + "__p.push('";
-	        })
-	        .replace(/\r/g, '\\r')
-	        .replace(/\n/g, '\\n')
-	        .replace(/\t/g, '\\t')
-	        + "');}return __p.join('');";
-	
-	func = new Function( 'obj', tmpl );
-	return data ? func( data ) : func;
-	
-};
+		//
+		// JavaScript micro-templating, similar to John Resig's implementation.
+		// Default implementation by underscore.js.
+		// https://github.com/documentcloud/underscore
+		//
+		tmpl = 'var __p=[],print=function(){__p.push.apply(__p,arguments);};' +
+			'with(obj||{}){__p.push(\'' +
+			str.replace(/\\/g, '\\\\')
+		        .replace(/'/g, "\\'")
+		        .replace( settings.interpolate, function( match, code ) {
+		          return "'," + code.replace(/\\'/g, "'") + ",'";
+		        })
+		        .replace( settings.evaluate || null, function( match, code ) {
+		          return "');" + code.replace(/\\'/g, "'")
+		                             .replace(/[\r\n\t]/g, ' ') + "__p.push('";
+		        })
+		        .replace(/\r/g, '\\r')
+		        .replace(/\n/g, '\\n')
+		        .replace(/\t/g, '\\t')
+		        + "');}return __p.join('');";
 
-// Load a html template from a URL, parsing it with the provided data.
-// If data is not provided, it returns the template string.
-//
-Wysiwyg.template.load = function( tplUrl, tplData, ajaxOptions ){
+		func = new Function( 'obj', tmpl );
+		return data ? func( data ) : func;
+	},
 	
-	var options = {
-		url:      tplUrl,
-		dataType: 'html',
-		success:  function(data){
-			
-			if ( tplData ){
-				return Wysiwyg.template.parse( data, tplData );
-			} else {
-				return tplData;
-			}
+	// Load a html template from a URL and caches the raw data for later.
+	//
+	load: function( tplURL, ajaxOptions ){
+		var options;
+		if ( Wysiwyg.template.cache[tplURL] !== undefined ){
+			return Wysiwyg.template.cache[tplURL];
 		}
-	};
+		
+		options = {
+			url:      tplURL,
+			dataType: 'html',
+			success:  function(data){
+				Wysiwyg.template.cache[tplURL] = data;				
+			}
+		};
+
+		if ( ajaxOptions ){
+			options = $.extend( options, ajaxOptions );
+		}
+
+		$.ajax( options );
+		return true;
+	},
 	
-	if ( ajaxOptions ){
-		options = $.extend( options, ajaxOptions );
+	// Store a named template for later use.
+	//
+	store: function( name, template ){
+		Wysiwyg.templateData[name] = template;
+		return template;
+	},
+	// Retrive a stored template, optionally parsing it with the provided data.
+	//
+	fetch: function( name, data ){
+		var tpl = Wysiwyg.template.store[name];
+		if( !data ){
+			return tpl;
+		}else{
+			return Wysiwyg.template.parse( tpl, data );
+		}
 	}
 	
-	$.ajax( options );
 	
-};
-
-// Store a template for later use. This way templates can be provided without pre-load functionality, and retrieved
-// as necessary for dialogs or other use.
-//
-Wysiwyg.template.store = function( name, template ){
-	Wysiwyg.templateData[name] = template;
-	return template;
-};
-
-// Retrive a stored template, optionally parsing it with the provided data.
-//
-Wysiwyg.template.fetch = function( name, data ){
-	var tpl = Wysiwyg.templateData[name];
-	if( !data ){
-		return tpl;
-	}else{
-		return Wysiwyg.template.parse( tpl, data );
-	}
-};
+});
