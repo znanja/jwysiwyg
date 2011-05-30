211d65a13f8ae53e9db133a1f7a04303a6511dd5
frost-nzcr4 <frost-nzcr4@gmail>
2011-05-30 20:10:56 +0400

    [fileManager] added utf8

--- plugins/fileManager/handlers/PHP/file-manager.php
+++ plugins/fileManager/handlers/PHP/file-manager.php
@@ -30,6 +30,11 @@ if (true) { // Debug.
 	ini_set('log_errors', 'On');
 }
 
+if (extension_loaded('mbstring')) {
+	mb_internal_encoding('UTF-8');
+	mb_regex_encoding('UTF-8');
+}
+
 require_once 'common.php';
 require_once 'handlers.php';
 
